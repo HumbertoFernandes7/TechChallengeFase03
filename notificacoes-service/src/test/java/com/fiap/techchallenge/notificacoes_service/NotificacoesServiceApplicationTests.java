f00e8993bf4af77cd67901df891722f08432b25e
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-11-05 18:27:10 -0300

    Adicionando novo servico de notificação

---
+++ notificacoes-service/src/test/java/com/fiap/techchallenge/notificacoes_service/NotificacoesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.notificacoes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacoesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
