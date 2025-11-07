a59d4343d44cda38eedcfe7fa62541b4e496942f
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-11-07 14:24:08 -0300

    Adicionando serviço de email

--- notificacoes-service/src/main/java/com/fiap/techchallenge/notificacoes/services/ConsultaConsumerService.java
+++ notificacoes-service/src/main/java/com/fiap/techchallenge/notificacoes/services/ConsultaConsumerService.java
@@ -1,28 +1,24 @@
 package com.fiap.techchallenge.notificacoes.services;
 
 import com.fiap.techchallenge.notificacoes.dtos.NotificacaoDTO;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ConsultaConsumerService {
 
     public static final String CONSULTA_QUEUE_NAME = "fila.consultas.notificacao";
-
     private static final Logger logger = LoggerFactory.getLogger(ConsultaConsumerService.class);
+
+    private final EmailService emailService;
 
     @RabbitListener(queues = CONSULTA_QUEUE_NAME)
     public void consumirMensagem(NotificacaoDTO notificacao) {
-        logger.info("-------------------------------------------");
-        logger.info("Nova notificação de consulta recebida!");
-        logger.info("Status: {}", notificacao.getStatus());
-        logger.info("Enviando lembrete para: {}", notificacao.getEmailPaciente());
-        logger.info("Paciente: {}", notificacao.getNomePaciente());
-        logger.info("Médico: {}", notificacao.getNomeMedico());
-        logger.info("Data: {}", notificacao.getDataHoraConsulta());
-        logger.info("-------------------------------------------");
-
+        logger.info("Mensagem recebida da fila. Acionando serviço de e-mail...");
+        emailService.sender(notificacao);
     }
 }
---
+++ notificacoes-service/src/main/java/com/fiap/techchallenge/notificacoes/services/EmailService.java
@@ -0,0 +1,40 @@
+package com.fiap.techchallenge.notificacoes.services;
+
+import com.fiap.techchallenge.notificacoes.dtos.NotificacaoDTO;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    private final JavaMailSender mailSender;
+
+    public void sender(NotificacaoDTO notificacaoDTO) {
+        try {
+            String corpoEmail = "Olá!\n\nUm lembrete foi enviado:\n\n" +
+                    "Paciente: " + notificacaoDTO.getNomePaciente() + "\n" +
+                    "Data: " + notificacaoDTO.getDataHoraConsulta() + "\n" +
+                    "Medico: " + notificacaoDTO.getNomeMedico() + "\n" +
+                    "Status: " + notificacaoDTO.getStatus() + "\n\n" +
+                    "Atenciosamente,\nEquipe FIAP";
+
+            SimpleMailMessage mensagem = new SimpleMailMessage();
+            mensagem.setFrom("hnino201333@gmail.com");
+            mensagem.setTo(notificacaoDTO.getEmailPaciente());
+            mensagem.setSubject("Lembre de consulta");
+            mensagem.setText(corpoEmail);
+
+            mailSender.send(mensagem);
+        } catch (Exception e) {
+            logger.error("Falha ao enviar email:" + e.getMessage());
+            throw new RuntimeException("Erro ao enviar e-mail: " + e.getMessage());
+        }
+    }
+}
